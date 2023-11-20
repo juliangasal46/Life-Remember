a85790eae6b8b4e519ad36c155eebf188e5fb023
juliangasal46 <juliangasal46@gmail.com>
2023-11-20 13:46:14 +0100

    V.0.0.3: Data storage OK

--- app/src/main/java/com/example/life_remember/MainActivity.java
+++ app/src/main/java/com/example/life_remember/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -31,16 +32,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        leerBBDD();
-
-        rellenarRecyclerView(); // Creamos tareas por defecto
         // Si el usuario quiere añadir una tarea, estas se borran
+
+        leerFicheros();
 
         // Ahora tenemos que pasarle los datos al recycler view
         RecyclerView recyclerView = findViewById(R.id.vwRecyclerView);
         Rcv_Adapter adaptadorTareas = new Rcv_Adapter(this, arrayTareas);
         recyclerView.setAdapter(adaptadorTareas);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
 
         btnAddRecordatorio = findViewById(R.id.imgSettings); // Pulsable
 
@@ -64,14 +66,17 @@ public class MainActivity extends AppCompatActivity {
 
                     // guardar datos que lleguen en shared preffereneces
                     // lanzar siguiente alert dialog
-                    if(etTitulo.getText().length() > 0){
+                    if (etTitulo.getText().length() > 0) {
+
+                        escribirRecordatorios(etTitulo.getText().toString(), "pito", "pito");
 
                     } else {
                         Toast.makeText(MainActivity.this, "No puedes relenar una tarea con el título vacío", Toast.LENGTH_SHORT).show();
                     }
                 });
 
-                alertDialogBuilder.setNegativeButton("Cancelar", (dialog, which) -> {});
+                alertDialogBuilder.setNegativeButton("Cancelar", (dialog, which) -> {
+                });
                 alertDialogBuilder.setView(dialogLayout);
                 alertDialogBuilder.show();
             } // onClick
@@ -79,48 +84,91 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Cuando presiones que salga la opción de modificar ? o eliminar
-    private void rellenarRecyclerView(){
+    /* private void rellenarRecyclerView() {
 
-        String[] arrayTitulos = {"Prueba1","Prueba2","Prueba3"};
+        String[] arrayTitulos = {"Prueba1", "Prueba2", "Prueba3"};
         String[] arrayDescripciones = {"Desc1", "Desc2", "Desc3"};
         String[] arrayRecordatorios = {"Rec1", "Rec2", "Rec3"};
 
-        for(int i=0; i<arrayTitulos.length; i++){
+        for (int i = 0; i < arrayTitulos.length; i++) {
             arrayTareas.add(new Tarea(
                     arrayTitulos[i],
                     arrayDescripciones[i],
                     arrayRecordatorios[i]));
         } // Añadimos en el array de tareas las 3 tareas por defecto
+    }*/
+
+
+    private boolean cheaquearExistenciaFichero(String[] archivos, String buscarArchivo) {
+        for (int i = 0; i < archivos.length; i++) {
+            if (buscarArchivo.equals(archivos[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
-    private void leerBBDD(){
+    private void leerFicheros() {
+
+        String[] archivos = fileList(); // Lista con todos los fichers
+
+        if (cheaquearExistenciaFichero(archivos, "bbdd_almacenar_tareas.txt")) {
+
+            try {
+
+                // Si existe leemos el documento
+                InputStreamReader isr = new InputStreamReader(openFileInput("bbdd_almacenar_tareas.txt"));
+                BufferedReader br = new BufferedReader(isr);
+                String linea = br.readLine();
+
+                String guardarLineas = "";
+                String []tareaSpliteada;
+
+                // Mientras que haya algo que leer en la línea -> Sigue
+                while (linea != null) {
+
+                    guardarLineas = linea;
+                    tareaSpliteada = guardarLineas.split("_");
+
+                    arrayTareas.add(new Tarea(tareaSpliteada[0], tareaSpliteada[1], tareaSpliteada[2]));
+                    // Hacer split por seccion
+
+                    // guardarLineas = guardarLineas + linea + "\n";
+                    Toast.makeText(getApplicationContext(), "Nardo -> " + guardarLineas + "\n", Toast.LENGTH_SHORT).show();
+                    linea = br.readLine();
+                }
+
+                br.close();
+                isr.close();
+
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public void escribirRecordatorios(String tituloTarea, String descTarea, String fechaRecordar) {
 
         try {
-            BufferedReader fin = new BufferedReader(new InputStreamReader(openFileInput("bbdd_almacenar_tareas.txt")));
-            String texto = fin.readLine();
-            Toast.makeText(this, "TEXTO EN EL ARCHIVO: " + texto, Toast.LENGTH_LONG).show();
-            fin.close();
-        } catch(Exception ex) {
-            // No existe el fichero
-            Toast.makeText(getApplicationContext(), "Excepción fichero" , Toast.LENGTH_SHORT).show();
-            ex.getMessage();
+
+            OutputStreamWriter osw = new OutputStreamWriter(openFileOutput("bbdd_almacenar_tareas.txt", Activity.MODE_PRIVATE));
+            // Mode private es para que solo esta función pueda acceder a ese fichero de forma escritura
+
+            // EL FORMATO SERÁ -> TITULO_DESC_FECHARECORDAR <- Lo guarda por lineas entonces no hace falta simbolo al final
+            osw.write(tituloTarea + "_" + descTarea + "_" + fechaRecordar);
+            osw.flush();
+            osw.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-    } // leerBBDD
-
-    private void crearFicheroBBDD(){
-
-        File file = new File("bbdd_almacenar_tareas.txt");
-
-        try {
-            FileOutputStream fos = openFileOutput("bbdd_almacenar_tareas.txt", Context.MODE_PRIVATE);
-            // You can write initial content to the file if needed
-            // fos.write("Initial content".getBytes());
-            fos.close(); // Close the FileOutputStream when done
-            Toast.makeText(getApplicationContext(), "Fichero creado correctamente", Toast.LENGTH_SHORT).show();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Error al crear el fichero", Toast.LENGTH_SHORT).show();
-        }
-    } // crearFicheroBBDD
+    }
 }
