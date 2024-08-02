2c5add11ec3d254563dc539d097a88ee50546ed8
juliangasal46 <juliangasal46@gmail.com>
2024-08-02 12:53:42 +0200

    Algunos bugs solucionados

--- app/src/main/java/com/example/life_remember/All_Chores_NoFilter.java
+++ app/src/main/java/com/example/life_remember/All_Chores_NoFilter.java
@@ -13,13 +13,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class All_Chores_NoFilter extends AppCompatActivity {
 
 
     private ArrayList<Tarea> arrayListTareas = new ArrayList<>();
-
+    ArrayList<Tarea> filteredTareas = new ArrayList<>();
     private ImageView btnGoBack;
 
     @Override
@@ -77,8 +80,6 @@ public class All_Chores_NoFilter extends AppCompatActivity {
 
                         arrayListTareas.add(new Tarea(tareaSpliteada[0], tareaSpliteada[1], tareaSpliteada[2]));
                         linea = br.readLine();
-
-                        Toast.makeText(this, tareaSpliteada[0], Toast.LENGTH_SHORT).show();
                     }
 
                     br.close();
@@ -96,10 +97,41 @@ public class All_Chores_NoFilter extends AppCompatActivity {
 
     private void generarRecyclerView(ArrayList<Tarea> arrayListTareas){
 
+        ordenarTareas();
         // Ahora tenemos que pasarle los datos al recycler view
         RecyclerView recyclerView = findViewById(R.id.vwRecyclerView_AllChores);
-        Rcv_Adapter adaptadorTareas = new Rcv_Adapter(this, arrayListTareas);
+        Rcv_Adapter adaptadorTareas = new Rcv_Adapter(this, filteredTareas);
         recyclerView.setAdapter(adaptadorTareas);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void ordenarTareas() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy-HH:mm");
+        int n = arrayListTareas.size();
+
+        // Método de Burbuja para ordenar tareas por fecha y hora
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                try {
+                    // Obtener las fechas y horas de las tareas
+                    Date fechaHoraActual = dateFormat.parse(arrayListTareas.get(j).getTiempo_recuerdo());
+                    Date fechaHoraSiguiente = dateFormat.parse(arrayListTareas.get(j + 1).getTiempo_recuerdo());
+
+                    // Comparar las fechas y horas
+                    if (fechaHoraActual.after(fechaHoraSiguiente)) {
+                        // Intercambiar las tareas si están en el orden incorrecto
+                        Tarea temp = arrayListTareas.get(j);
+                        arrayListTareas.set(j, arrayListTareas.get(j + 1));
+                        arrayListTareas.set(j + 1, temp);
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    // Manejar errores de parsing
+                }
+            }
+        }
+
+        // Puedes ahora agregar las tareas ordenadas a filteredTareas si es necesario
+        filteredTareas.addAll(arrayListTareas);
     }
 }
--- app/src/main/java/com/example/life_remember/MainActivity.java
+++ app/src/main/java/com/example/life_remember/MainActivity.java
@@ -26,8 +26,12 @@ import android.widget.Toast;
 import android.app.AlarmManager;
 
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView btnAddRecordatorio, btnAbrirTodasTareas;
     private DatePicker datePicker;
     ArrayList<Tarea> arrayTareas = new ArrayList<>();
+    ArrayList<Tarea> filteredTareas = new ArrayList<>();
     private String fechaModificada = "";
     private String fechaEditada = "";
 
@@ -99,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
                         fechaEditada = vwDatePicker_SelectNewFecha.getDayOfMonth() + "/"
                                 + mes + "/" + vwDatePicker_SelectNewFecha.getYear();
+
                         tvNewFecha.setText(fechaEditada);
 
                         // Cuando cambie el usuario la fecha, que se actualice automaticamente en el text view
@@ -148,10 +154,10 @@ public class MainActivity extends AppCompatActivity {
                             int hora_edit = tpTimePicker_edit.getHour();
                             int minutos_edit = tpTimePicker_edit.getMinute();
                             String formatoHora = hora_edit + ":" + minutos_edit;
-                            String formatoHora_chequeado = ceroIzquierda(formatoHora);
+                            String formatoHora_chequeado = ceroIzquierdaHoras(formatoHora);
 
                             // TODO: Vigilar
-                            if(compararHoras(hora_edit, minutos_edit)){
+                            if (compararHoras(hora_edit, minutos_edit)) {
                                 newEditHora.setText(formatoHora_chequeado);
                             }
 
@@ -252,6 +258,8 @@ public class MainActivity extends AppCompatActivity {
                                                             monthOfYear + "/" +
                                                             year;
 
+
+
                                                     TextView tvNewFecha = dialogLayoutSelectFecha.findViewById(R.id.tvNewFecha);
 
                                                     tvNewFecha.setText(fechaModificada);
@@ -279,6 +287,8 @@ public class MainActivity extends AppCompatActivity {
                                     // Quiere decir que no ha cambiado la fecha, es la del día actual
                                     String fechaActual = etFechaActualSistema.getText().toString();
 
+
+
                                     AlertDialog.Builder alertDialogBuilder4 = new AlertDialog.Builder(MainActivity.this);
                                     LayoutInflater inflater4 = getLayoutInflater();
                                     View dialogLayout4 = inflater4.inflate(R.layout.alert_newchore_hora, null);
@@ -290,13 +300,13 @@ public class MainActivity extends AppCompatActivity {
                                         String formatoHora = "";
                                         String hora_chequeada_ceros = "";
 
-                                        if(!compararHoras(hora, minutos)){
+                                        if (!compararHoras(hora, minutos)) {
                                             Toast.makeText(MainActivity.this, "La hora puesta es más pequeña que la hora actual, cambiala a la que quieras :D", Toast.LENGTH_LONG).show();
                                             formatoHora = "23:59";
-                                            hora_chequeada_ceros = ceroIzquierda(formatoHora);
+                                            hora_chequeada_ceros = ceroIzquierdaHoras(formatoHora);
                                         } else {
                                             formatoHora = hora + ":" + minutos;
-                                            hora_chequeada_ceros = ceroIzquierda(formatoHora);
+                                            hora_chequeada_ceros = ceroIzquierdaHoras(formatoHora);
                                         }
 
                                         // Por último, antes de guardar los datos, lanzaremos un diálogo de confirmación para
@@ -338,8 +348,6 @@ public class MainActivity extends AppCompatActivity {
                                             // Creamos las notificaciones
                                             // TODO: Crear notificaciones
                                             lanzarNotificacion();
-                                            Toast.makeText(MainActivity.this, "La notificación se ha creado", Toast.LENGTH_SHORT).show();
-
                                         });
 
                                         alertDialogBuilder5.setNegativeButton("Cancelar", (dialog5, which5) -> {
@@ -548,8 +556,7 @@ public class MainActivity extends AppCompatActivity {
                 tarea.getTiempo_recuerdo();
 
         try (BufferedReader bufrd = new BufferedReader(new InputStreamReader(openFileInput("bbdd_almacenar_tareas.txt")));
-             BufferedWriter bw = new BufferedWriter(new FileWriter(new File(getFilesDir(), "temp_bbdd_almacenar_tareas.txt"))))
-        {
+             BufferedWriter bw = new BufferedWriter(new FileWriter(new File(getFilesDir(), "temp_bbdd_almacenar_tareas.txt")))) {
             String line;
             while ((line = bufrd.readLine()) != null) {
                 if (!(line.equals(cadenaBuscarEnFichero))) {
@@ -591,30 +598,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public String ceroIzquierda(String horaCompleta){
+    public String ceroIzquierdaHoras(String horaCompleta) {
 
         // Como es una hora, hay que hacer split por los dos sitios
         String[] partesHora = horaCompleta.split(":");
         int horas = Integer.parseInt(partesHora[0]);
         int minutos = Integer.parseInt(partesHora[1]);
 
-        if(horas >= 0 && horas <= 9){
+        if (horas >= 0 && horas <= 9) {
 
-            if(minutos >= 0 && minutos <= 9){
+            if (minutos >= 0 && minutos <= 9) {
                 return "0" + horas + ":0" + minutos;
             }
 
             return "0" + horas + ":" + minutos;
         }
 
-        if(minutos >= 0 && minutos <= 9){
+        if (minutos >= 0 && minutos <= 9) {
             return horas + ":0" + minutos;
         }
 
         return horas + ":" + minutos;
     }
 
-    public String cogerHoraSistema(){
+    //  Mejorar porque se buguea, parece que solamente hará falta cuando se compare 
+    public String ceroIzquierdaFecha(String fechaCompleta){
+
+        fechaCompleta = fechaCompleta.split("-")[0];
+
+        String dia = fechaCompleta.split("/")[0];
+        String mes = fechaCompleta.split("/")[1];
+        String ano = fechaCompleta.split("/")[2];
+
+        if(Integer.parseInt(dia) < 9 || Integer.parseInt(dia) == 0){
+            dia = "0" + dia;
+        }
+
+        if(Integer.parseInt(mes) < 9 || Integer.parseInt(mes) == 0){
+            mes = "0" + mes;
+        }
+
+        if(Integer.parseInt(ano) < 9 || Integer.parseInt(ano) == 0){
+            ano = "0" + ano;
+        }
+
+        return dia + "/" + mes + "/" + ano;
+    }
+
+    public String cogerHoraSistema() {
         // Cuando se crea o edita una tarea, la hora tiene que ser mayor que la hora actual
 
         Calendar verHorasActuales = Calendar.getInstance();
@@ -622,27 +653,38 @@ public class MainActivity extends AppCompatActivity {
         int minutoActual = verHorasActuales.getTime().getMinutes();
         String minutoFormateado = "";
 
-        if(minutoActual < 10){
+        if (minutoActual < 10) {
             minutoFormateado = "0" + minutoActual;
         } else {
             minutoFormateado = Integer.toString(minutoActual);
         }
 
-        return horaActual+":"+minutoFormateado;
+        return horaActual + ":" + minutoFormateado;
     }
 
-    public boolean compararHoras(int horaCogida, int minutoCogido){
+    // Si falso = mismas horas
+    public boolean compararHoras(int horaCogida, int minutoCogido) {
 
         String horaCompletaSistema = cogerHoraSistema();
         int horaSistema = Integer.parseInt(horaCompletaSistema.split(":")[0]);
         int minutoSistema = Integer.parseInt(horaCompletaSistema.split(":")[1]);
 
+        // Obtener la fecha actual del sistema
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String fechaSistema = dateFormat.format(new Date());
 
-        if(horaCogida < horaSistema || minutoCogido < minutoSistema){
-            Toast.makeText(this, "La hora introducida es más pequeña que la actual", Toast.LENGTH_SHORT).show();
-            return false;
+        if(ceroIzquierdaFecha(cogerFechaActual(datePicker)).equals(fechaSistema)){
+
+            Toast.makeText(this, "La fecha es del pasado, no podemos poner una notificación :c, te la ponemos a día de hoy :D", Toast.LENGTH_SHORT).show();
+
+            if (horaCogida < horaSistema || minutoCogido < minutoSistema) {
+                return false;
+            } else {
+                return true;
+            }
+        } else{
+            return true;
         }
-        return true;
     }
 
 
@@ -691,23 +733,86 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-    private void lanzarNotificacion(){
 
-        // Se va a comparar por hora, porque la fecha se filtra cuando se muestra
+    private void lanzarNotificacion() {
+
+        /* String dia = "";
+        String mes = "";
+        String ano = "";
+        String hora_edit = "";
+        String hora = "";
+        String minutos = "";
+
         for(Tarea tarea : arrayTareas){
             String fecha_split = tarea.getTiempo_recuerdo().split("-")[0];
 
-            String dia = fecha_split.split("/")[0];
-            String mes = fecha_split.split("/")[1];
-            String ano = fecha_split.split("/")[2];
+            dia = fecha_split.split("/")[0];
+            mes = fecha_split.split("/")[1];
+            ano = fecha_split.split("/")[2];
 
-            String hora_edit = tarea.getTiempo_recuerdo().split("-")[1];
-            String hora = hora_edit.split(":")[0];
-            String minutos = hora_edit.split(":")[1];
+            hora_edit = tarea.getTiempo_recuerdo().split("-")[1];
+            hora = hora_edit.split(":")[0];
+            minutos = hora_edit.split(":")[1];
+        }
 
-            scheduleNotification(Integer.parseInt(ano), Integer.parseInt(mes), Integer.parseInt(dia),
-                    Integer.parseInt(hora), Integer.parseInt(minutos), 0 ,0 );
+        scheduleNotification(Integer.parseInt(ano), Integer.parseInt(mes), Integer.parseInt(dia),
+                Integer.parseInt(hora), Integer.parseInt(minutos), 0 ,0 );*/
 
+        // Enseña todas las tareas sin filtrado, para ir una por una programando la notificación
+        // Han de ordenarse por fecha
+        /* for(Tarea tarea : arrayTareasNoFiltrado){
+            Toast.makeText(this, tarea.getTitulo(), Toast.LENGTH_SHORT).show();
+        }*/
+        // ordenadoFechas();
+    }
+
+    private void ordenadoFechas() {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            // Fecha de referencia para comparar (por ejemplo, comparar si es después de esta fecha)
+            Date fechaReferencia = dateFormat.parse("01-01-2024");
+
+            // Limpiar filteredTareas antes de llenarla
+            filteredTareas.clear();
+
+            // Iterar y comparar fechas
+            for (Tarea tarea : arrayTareas) {
+
+                String fecha_split = tarea.getTiempo_recuerdo().split("-")[0];
+
+                String dia = fecha_split.split("/")[0];
+                String mes = fecha_split.split("/")[1];
+                String ano = fecha_split.split("/")[2];
+
+                String hora_edit = tarea.getTiempo_recuerdo().split("-")[1];
+                String hora = hora_edit.split(":")[0];
+                String minutos = hora_edit.split(":")[1];
+
+                Date fechaTarea = dateFormat.parse(dia + "-" + mes + "-" + ano);
+
+                // Comparar la fecha de la tarea con la fecha de referencia
+                if (fechaTarea.after(fechaReferencia)) {
+                    filteredTareas.add(tarea);
+                    Toast.makeText(this, "Se ha comparado bien", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, fechaTarea + " = " + "01-01-2024", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Algo ha salido mal", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, fechaTarea + " = " + "01-01-2024", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            // Mostrar mensaje de confirmación
+            Toast.makeText(this, "Ya se ha ordenado todo, enseñando", Toast.LENGTH_SHORT).show();
+
+            // Mostrar las tareas filtradas
+            for (Tarea tarea : filteredTareas) {
+                Toast.makeText(this, tarea.getTitulo() + " - " + tarea.getTiempo_recuerdo(), Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (ParseException e) {
+            // Manejar errores de parsing
+            e.printStackTrace();
+            Toast.makeText(this, "Error al parsear la fecha", Toast.LENGTH_SHORT).show();
         }
     }
 }
